6e3d2a38bca7a7dcb70c53946d73fa3570a129c8
Krish1314 <krish09755650065@gmail.com>
2026-05-05 21:32:46 +0530

    fix: add missing import for EntityGraph

--- backend_java/src/main/java/com/uniformly/cart/CartItemRepository.java
+++ backend_java/src/main/java/com/uniformly/cart/CartItemRepository.java
@@ -1,6 +1,7 @@
 package com.uniformly.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import java.util.List;
 import java.util.Optional;
